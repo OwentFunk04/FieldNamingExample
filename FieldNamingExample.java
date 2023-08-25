4bb986825c61fd20782bf3141add50cd0361c3d8
Owen Funk <owentfunk@icloud.com>
2023-08-24 20:45:11 -0400

    commit

---
+++ FieldNamingExample.java
@@ -0,0 +1,11 @@
+public class FieldNamingExample {
+    int legalField1;
+    String legalField2;
+
+    public static void main(String[] args) {
+        FieldNamingExample example = new FieldNamingExample();
+
+        System.out.println(example.legalField1);
+        System.out.println(example.legalField2);
+    }
+}
